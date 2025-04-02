c7094085d67962ea07d293ed7c168fa81d202367
triyank-pedalsup <triyank.bhavaliya@pedalsup.com>
2025-04-02 18:00:39 +0530

    hlo

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/userCrud")
+mongoose.connect("mongodb://localhost:27017/userCrud", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("Connected to MongoDB successfully");
     })
     .catch((err) => {
         console.log("Error connecting to MongoDB:", err);
     });
+
+app.get("/test", (req, res) => {
+    res.send("Welcome to the Express.js server!");
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
